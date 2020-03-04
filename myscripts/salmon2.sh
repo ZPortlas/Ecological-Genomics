160192454f20bbfea50f0516dee869b7d8f88f66
ZPortlas <42742272+ZPortlas@users.noreply.github.com>
2020-03-04 10:56:45 -0500

    fix salmon2

--- myscripts/salmon2.sh
+++ myscripts/salmon2.sh
@@ -6,7 +6,7 @@ for file in NOR*C*.fq
 
 do
 
-salmon quant -i /data/project_data/RS_RNASeq/ReferenceTranscriptome/Pabies_HC_index -l A -r ${file} --validateMappings -seqBias -o /data/project_data/RS_RNASeq/salmon/HCmapping/${file}
+salmon quant -i /data/project_data/RS_RNASeq/ReferenceTranscriptome/Pabies_HC_index -l A -r ${file} --validateMappings --seqBias -o /data/project_data/RS_RNASeq/salmon/HCmapping/${file}
 
 done
 
@@ -14,6 +14,6 @@ for file in NOR*D*.fq
 
 do
 
-salmon quant -i /data/project_data/RS_RNASeq/ReferenceTranscriptome/Pabies_HC_index -l A -r ${file} --validateMappings -seqBias -o /data/project_data/RS_RNASeq/salmon/HCmapping/${file}
+salmon quant -i /data/project_data/RS_RNASeq/ReferenceTranscriptome/Pabies_HC_index -l A -r ${file} --validateMappings --seqBias -o /data/project_data/RS_RNASeq/salmon/HCmapping/${file}
 
 done
