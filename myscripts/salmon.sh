c0672e22056df93f7146e72bed0359d451b83aa3
ZPortlas <42742272+ZPortlas@users.noreply.github.com>
2020-03-04 09:39:46 -0500

    Update salmon.sh

--- myscripts/salmon.sh
+++ myscripts/salmon.sh
@@ -4,7 +4,7 @@ for file in /data/project_data/RS_RNASeq/fastqc/cleanreads/NOR*C*.cl.fq
 
 do
 
-salmon quant -i /data/project_data/RS_RNASeq/ReferenceTranscriptome/Pabies_HC27_index -l A -r ${file} --validateMappings -o transcripts_quant -o /data/project_data/RS_RNASeq/salmon/cleanedreads
+salmon quant -i /data/project_data/RS_RNASeq/ReferenceTranscriptome/Pabies_HC27_index -l A -r ${file} --validateMappings -o /data/project_data/RS_RNASeq/salmon/cleanedreads
 
 done
 
@@ -12,6 +12,6 @@ for file in /data/project_data/RS_RNASeq/fastqc/cleanreads/NOR*D*.cl.fq
 
 do
 
-salmon quant -i /data/project_data/RS_RNASeq/ReferenceTranscriptome/Pabies_HC27_index -l A -r ${read} --validateMappings -o transcripts_quant -o /data/project_data/RS_RNASeq/salmon/cleanedreads
+salmon quant -i /data/project_data/RS_RNASeq/ReferenceTranscriptome/Pabies_HC27_index -l A -r ${read} --validateMappings -o /data/project_data/RS_RNASeq/salmon/cleanedreads
 
 done
